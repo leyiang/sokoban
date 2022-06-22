b39c09ceba815b3f7d67ad3da16a6fdae28661d3
Yiang <yiangforwork@gmail.com>
2022-06-23 05:17:51 +0800

    Boundary Check

--- src/core/Game.js
+++ src/core/Game.js
@@ -1,9 +1,10 @@
-import Character from "./Character.js";
+import Player from "./Player.js";
 import Keyboard from "./Keyboard.js";
+import config from "../js/config.js";
 
 export default class Game {
     constructor() {
-        this.player = new Character();
+        this.player = new Player();
         this.keyboard = new Keyboard;
     }
 
@@ -14,5 +15,14 @@ export default class Game {
     init() {
         this.player.init();
         this.keyboard.listen();
+    }
+
+    validPosition( x, y ) {
+        if( x < 0 ) return false;
+        if( y < 0 ) return false;
+        if( x > config.col - 1 ) return false;
+        if( y > config.row - 1 ) return false;
+
+        return true;
     }
 }
--- src/core/Player.js
+++ src/core/Player.js
@@ -1,27 +1,38 @@
 import config from "../js/config.js";
 import { path } from "./utils.js";
 
-export default class Character {
+export default class Player {
     constructor() {
         this.x = 5;
         this.y = 5;
     }
 
     init() {
-        game.keyboard.bind(["ArrowUp", "k"], () => {
-            this.y --;
+        game.keyboard.bind(["ArrowUp", "k", "w"], () => {
+            this.move(0, -1);
         });
 
-        game.keyboard.bind(["ArrowDown", "j"], () => {
-            this.y ++;
+        game.keyboard.bind(["ArrowDown", "j", "s"], () => {
+            this.move(0, 1);
         });
 
-        game.keyboard.bind(["ArrowLeft", "h"], () => {
-            this.x --;
+        game.keyboard.bind(["ArrowLeft", "h", "a"], () => {
+            this.move(-1, 0);
         });
-        game.keyboard.bind(["ArrowRight", "l"], () => {
-            this.x ++;
+
+        game.keyboard.bind(["ArrowRight", "l", "d"], () => {
+            this.move(1, 0);
         });
+    }
+
+    move( xDir = 0, yDir = 0 ) {
+        let newX = this.x + xDir;
+        let newY = this.y + yDir;
+
+        if( game.validPosition(newX, newY) ) {
+            this.x = newX;
+            this.y = newY;
+        }
     }
 
     render( c, view ) {
